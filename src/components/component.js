2a260ade20f45292620b056770d848c3ab7a03ca
Alan Coiradas <coiradasalan@gmail.com>
2023-06-15 19:29:35 -0300

    Se aplicaron los estilos correspondientes a las clases en el archivo App.js

--- src/components/component.js
+++ src/components/component.js
@@ -5,9 +5,9 @@ function Testimonio(props){
         <div className='contenedor-testimonio'>
             <img className='imagen-testimonio' src={require(`../images/testimonio-${props.imagen}.png`)} alt='Foto de persona'/>
             <div className='contenedor-texto-testimonio'>
-                <p><strong>{props.nombre}</strong> en {props.pais}</p>
-                <p><strong>{props.cargo}</strong> en {props.empresa}</p>
-                <p>"{props.testimonio}"</p>
+                <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className='cargo-testimonio'><strong>{props.cargo}</strong> en {props.empresa}</p>
+                <p className='texto-testimonio'>"{props.testimonio}"</p>
             </div>
         </div>
 
