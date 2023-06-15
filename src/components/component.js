885b978facaff8b0adbc18a284556fd7dcc81d33
Alan Coiradas <coiradasalan@gmail.com>
2023-06-15 19:22:40 -0300

    Se corrigieron errores de compilación que no permitían la visualización de la página

--- src/components/component.js
+++ src/components/component.js
@@ -1,12 +1,13 @@
-import '../styles/component-styles.css'
+import React from 'react'
+import '../styles/component-style.css'
 function Testimonio(props){
     return(
         <div className='contenedor-testimonio'>
-            <img className='imagen-testimonio' src={require(`../images/testimonio-{props.name}.png`)} alt='`Foto de ${props.name}`'/>
+            <img className='imagen-testimonio' src={require(`../images/testimonio-${props.imagen}.png`)} alt='Foto de persona'/>
             <div className='contenedor-texto-testimonio'>
                 <p><strong>{props.nombre}</strong> en {props.pais}</p>
                 <p><strong>{props.cargo}</strong> en {props.empresa}</p>
-                <p>{props.testimonio}</p>
+                <p>"{props.testimonio}"</p>
             </div>
         </div>
 
